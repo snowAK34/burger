624923723576d5e95edb4140980d7418203cd62f
Stacy Nowak <nowak.stacy@gmail.com>
2019-04-07 22:33:18 -0700

    initial file setup

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INTEGER(10) AUTO_INCREMENT PRIMARY KEY,
+    burger_name VARCHAR(60) NOT NULL,
+    devoured BOOLEAN DEFAULT false
+);
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Portobello mushroom veggie burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Bleu cheese bacon burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Black bean burger");
